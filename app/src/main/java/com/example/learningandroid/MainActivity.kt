c2cf5bb1dd37496428974d3f16a321c6b6a30f1e
Алексей Федоров <Fedorov.Alexey@live.ru>
2022-01-05 12:41:49 +0300

    Постигаю ведроид и котлин

--- app/src/main/java/com/example/learningandroid/MainActivity.kt
+++ app/src/main/java/com/example/learningandroid/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.learningandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+        val textView: TextView = findViewById(R.id.text_view)
+        textView.text=(1..6).random().toString()
+
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
     }
 }
