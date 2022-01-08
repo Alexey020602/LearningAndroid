c1aad8798e553d685addf51be585f33b0d9b8e4c
Алексей Федоров <Fedorov.Alexey@live.ru>
2022-01-08 10:48:34 +0300

    Продолжение

--- app/src/main/java/com/example/learningandroid/MainActivity.kt
+++ app/src/main/java/com/example/learningandroid/MainActivity.kt
@@ -3,18 +3,26 @@ package com.example.learningandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var diceImage:ImageView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
-        val textView: TextView = findViewById(R.id.text_view)
-        textView.text=(1..6).random().toString()
-
+        val randomInt=(1..6).random()
+        diceImage = findViewById(R.id.dice_image)
+        val drawableResource=when(randomInt){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 
     private fun rollDice() {
